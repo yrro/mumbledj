180f484caa07f30340d38fe4ab6a97cafca04fb4
Matthieu Grieger <matthieugrieger@gmail.com>
2015-12-16 20:19:22 -0800

    Added MockedAudioHandler

--- testutils/mocks.go
+++ testutils/mocks.go
@@ -8,6 +8,9 @@
 package testutils
 
 import (
+	"errors"
+	"strconv"
+
 	"github.com/matthieugrieger/mumbledj/audio"
 	"github.com/stretchr/testify/mock"
 )
@@ -25,5 +28,34 @@ func (m *MockedAudioTrack) Title() string {
 
 // Submitter is a method that returns the submitter of the track.
 func (m *MockedAudioTrack) Submitter() string {
-	return "Matt"
+	return "TestSubmitter"
+}
+
+// Service is a method that returns the service the track comes from.
+func (m *MockedAudioTrack) Service() string {
+	return "TestService"
+}
+
+// MockedAudioHandler is a mocked audio handler for testing purposes.
+type MockedAudioHandler struct {
+	audio.Handler
+	mock.Mock
+}
+
+// GetTracks returns a number of MockedAudioTracks specified with the num argument.
+// If num is 0 a test error is returned.
+func (m *MockedAudioHandler) GetTracks(num string) ([]audio.Track, error) {
+	var tracks []audio.Track
+	numTracks, _ := strconv.ParseInt(num, 10, 32)
+	if numTracks == 0 {
+		return nil, errors.New("Test error.")
+	}
+
+	i := 0
+	for i < int(numTracks) {
+		tracks = append(tracks, new(MockedAudioTrack))
+		i++
+	}
+
+	return tracks, nil
 }
