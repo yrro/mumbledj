307b70d0df67e1fd373c49d583a159934d2fcdfc
Matthieu Grieger <matthieugrieger@gmail.com>
2015-12-19 23:41:04 -0800

    Implemented help command and its unit tests

--- commands/help.go
+++ commands/help.go
@@ -8,6 +8,8 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/layeh/gumble/gumble"
 	"github.com/matthieugrieger/mumbledj/state"
 	"github.com/spf13/viper"
@@ -22,6 +24,11 @@ func (c *HelpCommand) Aliases() []string {
 	return viper.GetStringSlice("aliases.help")
 }
 
+// Description returns a description of the command.
+func (c *HelpCommand) Description() string {
+	return viper.GetString("descriptions.help")
+}
+
 // IsAdmin is a command that returns a bool that determines if a command is an
 // admin command or not.
 func (c *HelpCommand) IsAdmin() bool {
@@ -30,5 +37,37 @@ func (c *HelpCommand) IsAdmin() bool {
 
 // Execute executes the command with the given bot state, user, and arguments.
 func (c *HelpCommand) Execute(state *state.BotState, user *gumble.User, args ...string) (*state.BotState, string, bool, error) {
-	return nil, "", false, nil
+	commandString := "<b>%s</b> -- %s</br>"
+	regularCommands := ""
+	adminCommands := ""
+	totalString := ""
+	commander := NewCommander()
+
+	for _, command := range commander.Commands {
+		currentString := fmt.Sprintf(commandString, command.Aliases(), command.Description())
+		if command.IsAdmin() {
+			adminCommands += currentString
+		} else {
+			regularCommands += currentString
+		}
+	}
+
+	totalString = "<b>Commands:</b></br>" + regularCommands
+
+	isAdmin := false
+	if viper.GetBool("permissions.adminsenabled") {
+		for _, username := range viper.GetStringSlice("permissions.admins") {
+			if user.Name == username {
+				isAdmin = true
+			}
+		}
+	} else {
+		isAdmin = true
+	}
+
+	if isAdmin {
+		totalString += "</br><b>Admin Commands:</b></br>" + adminCommands
+	}
+
+	return nil, totalString, true, nil
 }
--- commands/help_test.go
+++ commands/help_test.go
@@ -6,3 +6,64 @@
  */
 
 package commands
+
+import (
+	"testing"
+
+	"github.com/layeh/gumble/gumble"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/suite"
+)
+
+type HelpCommandTestSuite struct {
+	Command HelpCommand
+	suite.Suite
+}
+
+func (suite *HelpCommandTestSuite) SetupSuite() {
+	viper.Set("aliases.help", []string{"help", "h"})
+	viper.Set("descriptions.help", "help")
+	viper.Set("permissions.help", false)
+	viper.Set("permissions.adminsenabled", true)
+	viper.Set("permissions.admins", []string{"Admin"})
+}
+
+func (suite *HelpCommandTestSuite) TestAliases() {
+	suite.Equal([]string{"help", "h"}, suite.Command.Aliases())
+}
+
+func (suite *HelpCommandTestSuite) TestDescription() {
+	suite.Equal("help", suite.Command.Description())
+}
+
+func (suite *HelpCommandTestSuite) TestIsAdmin() {
+	suite.False(suite.Command.IsAdmin())
+}
+
+func (suite *HelpCommandTestSuite) TestExecute() {
+	adminUser := new(gumble.User)
+	adminUser.Name = "Admin"
+
+	regularUser := new(gumble.User)
+	regularUser.Name = "NotAdmin"
+
+	state1, message1, isPrivateMessage1, err1 := suite.Command.Execute(nil, regularUser)
+
+	suite.Nil(state1, "No state should be returned since the state did not change.")
+	suite.NotEqual("", message1, "A message should be returned with the command descriptions.")
+	suite.True(isPrivateMessage1, "This should be a private message.")
+	suite.Nil(err1, "No error should be returned.")
+
+	state2, message2, isPrivateMessage2, err2 := suite.Command.Execute(nil, adminUser)
+
+	suite.Nil(state2, "No state should be returned since the state did not change.")
+	suite.NotEqual("", message2, "A message should be returned with the command descriptions.")
+	suite.True(isPrivateMessage2, "This should be a private message.")
+	suite.Nil(err2, "No error should be returned.")
+
+	suite.True(len(message2) > len(message1), "The response for an admin should have more commands listed.")
+}
+
+func TestHelpCommandTestSuite(t *testing.T) {
+	suite.Run(t, new(HelpCommandTestSuite))
+}
