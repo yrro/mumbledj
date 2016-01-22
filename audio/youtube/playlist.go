4cf0b3898c66734182f0a487ff0f56736513ea40
Matthieu Grieger <matthieugrieger@gmail.com>
2016-01-22 10:07:15 -0800

    Implemented YouTube playlists

--- audio/youtube/playlist.go
+++ audio/youtube/playlist.go
@@ -10,6 +10,7 @@ package youtube
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/antonholmquist/jason"
 	"github.com/layeh/gumble/gumble"
@@ -22,6 +23,7 @@ type Playlist struct {
 	Author    string
 	Title     string
 	Submitter string
+	Tracks    []*Track
 }
 
 // NewPlaylist returns a YouTube playlist struct with the provided ID.
@@ -48,6 +50,40 @@ func (p *Playlist) FetchMetadata() error {
 
 	p.Title, _ = value.GetString("items", "0", "snippet", "title")
 	p.Author, _ = value.GetString("items", "0", "snippet", "channelTitle")
+
+	// Fetch track metadata.
+	maxItemsPerPage := 50
+	maxTracks := 0
+	if maxTracks = viper.GetInt("general.maxtracksperplaylist"); maxTracks < 50 && maxTracks != 0 {
+		maxItemsPerPage = maxTracks
+	}
+	pageToken := ""
+	playlistInfo := Playlist{
+		ID:        p.ID,
+		Submitter: p.Submitter,
+	}
+	for len(p.Tracks) < maxTracks {
+		if response, err = http.Get(fmt.Sprintf("https://www.googleapi.com/youtube/v3/playlistItems?part=snippet&maxResults=%d&playlistId=%s&pageToken=%s&key=%s",
+			maxItemsPerPage, p.ID, pageToken, viper.GetString("api.youtubekey"))); err != nil {
+			return err
+		}
+		if value, err = jason.NewObjectFromReader(response.Body); err != nil {
+			return err
+		}
+
+		tracks, _ := value.GetValueArray("items")
+		for i := 0; (i < len(tracks)) && (len(p.Tracks) < maxTracks); i++ {
+			index := strconv.Itoa(i)
+			videoID, _ := value.GetString("items", index, "snippet", "resourceId", "videoId")
+			if track, err := NewTrack(p.Submitter, videoID, "", playlistInfo); err == nil {
+				track.FetchMetadata()
+				p.Tracks = append(p.Tracks, track)
+			}
+		}
+		if pageToken, err = value.GetString("nextPageToken"); err != nil {
+			break
+		}
+	}
 
 	return nil
 }
